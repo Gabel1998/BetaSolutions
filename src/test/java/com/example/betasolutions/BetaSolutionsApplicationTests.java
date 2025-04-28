98b5a0cf202918ae79b9f2a83c95629d93aff118
Andreas Gabel <andreassgabel@hotmail.com>
2025-04-28 13:10:24 +0200

    Initial commit

---
+++ src/test/java/com/example/betasolutions/BetaSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.betasolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaSolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
