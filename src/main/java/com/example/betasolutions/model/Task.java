70d49ad8264fbb5d171cb47d80a57674f4f2abb6
bixson <alexanderornb@gmail.com>
2025-05-01 16:48:17 +0200

    Implement user input validation - task 3.5
    # Conflicts:
    #	src/main/resources/templates/tasks/create.html

--- src/main/java/com/example/betasolutions/model/Task.java
+++ src/main/java/com/example/betasolutions/model/Task.java
@@ -1,15 +1,33 @@
 package com.example.betasolutions.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDateTime;
 
 public class Task {
     private Long id; //Primary Key
+
+    @NotNull(message = "Project ID is required")
     private Long projectId; //foreign key til projects
     private Integer subProjectId; //foreign key til subProjects
+
+    @NotBlank(message = "Task name cannot be empty")
+    @Size(max = 100, message = "Task name must be under 100 characters")
     private String name;
+
+    @Size(max = 500, message = "Description must be under 500 characters")
     private String description;
+
+    @NotNull(message = "Estimated hours is required")
+    @PositiveOrZero(message = "Estimated hours must be 0 or more")
     private Double estimatedHours;
+
+    @PositiveOrZero(message = "Actual hours must be 0 or more")
     private Double actualHours;
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
