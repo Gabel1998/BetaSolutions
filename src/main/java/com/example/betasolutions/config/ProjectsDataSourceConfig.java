df60f855cdc8edb7401f3f707ce9791ff4b04942
bixson <alexanderornb@gmail.com>
2025-05-12 13:25:31 +0200

    added not test to config classes

--- src/main/java/com/example/betasolutions/config/ProjectsDataSourceConfig.java
+++ src/main/java/com/example/betasolutions/config/ProjectsDataSourceConfig.java
@@ -4,9 +4,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import javax.sql.DataSource;
-
+@Profile("!test")
 @Configuration
 public class ProjectsDataSourceConfig {
 
