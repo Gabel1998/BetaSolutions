b1229a99ecb3201e79fa8b2689f750aec936324a
bixson <alexanderornb@gmail.com>
2025-05-12 13:07:01 +0200

    annual bugs

--- src/main/java/com/example/betasolutions/config/H2TestConfig.java
+++ src/main/java/com/example/betasolutions/config/H2TestConfig.java
@@ -1,18 +1,35 @@
 package com.example.betasolutions.config;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class H2TestConfig {
+
     @Bean
     @Primary
-    public PlatformTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
+    public DataSource h2DataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("org.h2.Driver")
+                .url("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1")
+                .username("sa")
+                .password("")
+                .build();
+    }
+
+    @Bean
+    @Primary
+    public JdbcTemplate h2JdbcTemplate(DataSource h2DataSource) {
+        return new JdbcTemplate(h2DataSource);
+    }
+
+    @Bean
+    public JdbcTemplate employeesJdbcTemplate(DataSource h2DataSource) {
+        return new JdbcTemplate(h2DataSource);
     }
 }
