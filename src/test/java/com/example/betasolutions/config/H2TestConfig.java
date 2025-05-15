b1988707eefda123a10121a40f66182076771347
bixson <alexanderornb@gmail.com>
2025-05-15 21:57:36 +0200

    Oops fixed accidental JPA config

--- src/test/java/com/example/betasolutions/config/H2TestConfig.java
+++ src/test/java/com/example/betasolutions/config/H2TestConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -11,11 +12,12 @@ import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 
 @TestConfiguration
+@Profile("test")
 public class H2TestConfig {
 
-    @Bean
+    @Bean(name = "projectsDataSource")
     @Primary
-    public DataSource h2DataSource() {
+    public DataSource projectsDataSource() {
         return DataSourceBuilder.create()
                 .driverClassName("org.h2.Driver")
                 .url("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1")
@@ -24,21 +26,31 @@ public class H2TestConfig {
                 .build();
     }
 
-    @Bean
-    @Primary
-    public JdbcTemplate h2JdbcTemplate(DataSource h2DataSource) {
-        return new JdbcTemplate(h2DataSource);
+    @Bean(name = "employeesDataSource")
+    public DataSource employeesDataSource() {
+        // same datasource for simplicity
+        return DataSourceBuilder.create()
+                .driverClassName("org.h2.Driver")
+                .url("jdbc:h2:mem:employeesdb;DB_CLOSE_DELAY=-1")
+                .username("sa")
+                .password("")
+                .build();
     }
 
-    // Provide a dummy JdbcTemplate to satisfy @Qualifier("employeesJdbcTemplate")
+    @Bean
+    @Primary
+    public JdbcTemplate jdbcTemplate(DataSource projectsDataSource) {
+        return new JdbcTemplate(projectsDataSource);
+    }
+
     @Bean(name = "employeesJdbcTemplate")
-    public JdbcTemplate employeesJdbcTemplate(DataSource h2DataSource) {
-        return new JdbcTemplate(h2DataSource);
+    public JdbcTemplate employeesJdbcTemplate(DataSource employeesDataSource) {
+        return new JdbcTemplate(employeesDataSource);
     }
 
     @Bean
     @Primary
-    public PlatformTransactionManager transactionManager(DataSource h2DataSource) {
-        return new DataSourceTransactionManager(h2DataSource);
+    public PlatformTransactionManager transactionManager(DataSource projectsDataSource) {
+        return new DataSourceTransactionManager(projectsDataSource);
     }
 }
